6cbc78c7765dcd708f1fcebf9edcf5bdf83b17ba
adamfowleruk <adamfowleruk@gmail.com>
2015-11-26 23:04:07 +0000

    updated files

--- release/src/MLCPlusPlus.cpp
+++ release/src/MLCPlusPlus.cpp
@@ -21,6 +21,37 @@ Connection* Connection::Instance(void) {
     return Connection::_instance;
 }
 
-void configure(const std::string& hostname, const std::string& port, const std::string& username, const std::string& password) {
-  // TODO
+void configure(const std::string& hostname, const std::string& port, const std::string& username, const std::string& password, bool usessl) {
+  _serverUrl = "http://" + hostname + ":" + port;
+  Credentials c(username, password);
+  _proxy.addCredentials(c)
+
+}
+
+Response get(std::string& uri) {
+  Response response = _proxy.Get(_serverUrl, "/v1/documents?uri=" + uri); // TODO escape URI for URL rules
+
+  return response;
+}
+
+// TODO XML version
+Response save(std::string& uri,web::json::value payload) {
+    //payload[utility::string_t("hello")] = web::json::value::string("world");
+
+  Response response = ap.Put(_serverUrl,
+    "/v1/documents?extension=json&uri=" + uri, // TODO directory (non uri) version TODO check for URL parsing
+    payload);
+  return response;
+}
+
+
+Response saveAll(std::string& uris[], web::json::value payload[]) {
+  // TODO multi part mime 
+}
+
+Response search(web::json::value searchQuery,std::string& options) {
+  Response response = ap.Post(_serverUrl,
+    "/v1/search?options=" + options, // TODO check for URL parsing
+    payload);
+  return response;
 }
--- release/src/MLCPlusPlus.hpp
+++ release/src/MLCPlusPlus.hpp
@@ -8,11 +8,31 @@
 
 class Connection {
     static Connection* _instance;
+    std::string& _serverUrl;
+    AuthenticatingProxy _proxy;
 
     Connection();
 public:
     static Connection* Instance(void);
     void configure(const std::string& hostname, const std::string& port, const std::string& username, const std::string& password);
+    // TODO anonymous connection support
+    // TODO Kerberos connection support
+    // TODO test BASIC and DIGEST and BASIC+DIGEST authentication methods
+
+    Response get(std::string& uri);
+    Response save(std::string& uri,web::json::value payload);
+    // TODO save JSON with directory, no uri
+    // TODO optional parameters (collection, security, etc.)
+    // TODO save XML with and without URI
+
+    // Multi part MIME saveAll function V7+
+    Response saveAll(std::string& uris[], web::json::value payload[]);
+    // TODO XML save all
+    // TODO multiple responses when paging in N number of doc transactions
+
+    Response search(web::json::value searchQuery,std::string& options);
+    // TODO search with options document
+    // TODO search with XML instead of JSON
 };
 
 #endif /* defined(__MLCPlusPlus__) */
