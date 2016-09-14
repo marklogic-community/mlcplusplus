f3ec8b8500b7da6d52122be4ad3a03ebdad441b6
adamfowleruk <adamfowleruk@gmail.com>
2016-09-14 21:44:03 +0100

    Regenerated API docs

--- developapi/search/pages_0.js
+++ developapi/search/pages_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
+  ['application_20developers_20guide_20for_20c_23',['Application Developers Guide for C#',['../page_csdev.html',1,'']]],
   ['application_20developers_20guide',['Application Developers Guide',['../page_devguide.html',1,'']]]
 ];
